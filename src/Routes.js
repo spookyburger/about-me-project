abcfb4d749b6f914d95b53154e53fc125b49b931
Brad Butler <butlergames@gmail.com>
2017-10-21 15:12:30 -0600

    Add files via upload

--- src/Routes.js
+++ src/Routes.js
@@ -7,34 +7,27 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
-import Background from './img/starsglow1.png'
 
 export default () => {
 
-const sectionStyle ={
-        backgroundImage: `url(${Background})`,
-        height: `100%`
-    }
-
-
     return (
         <div className="fullscreen">
-        <section style={ sectionStyle }>
             <div className="pagecontainer">
             <div className="pagebody">
-                <Header/>
                 <Router>
+                    <div>
+                    <Header/>
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route path="/projects" component={Projects} />
                         <Route path="/contact" component={Contact} />
                         <Route component={NotFound} />
                     </Switch>
+                    <Footer/>
+                    </div>
                 </Router>
-                <Footer/>
             </div>
             </div>
-        </section>
         </div>
     )
 }
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import logo from '../img/cropped-header.jpg'
 
 export default () => {
@@ -7,7 +8,7 @@ export default () => {
             <header>
                 <a href='/'><img src={logo} alt="Fry Ghost // Brad Butler"/></a>
                 <p>
-                    <a href='/'>Home</a><a href='/projects'>Projects</a><a href='/contact'>Contact</a>
+                    <Link to='/'>Home</Link><Link to='/projects'>Projects</Link><Link to='/contact'>Contact</Link>
                 </p>
             </header>
         </div>
